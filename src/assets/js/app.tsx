5ae5d702002f5837b7524e01b0e8f8e964c998ba
Gabriel Mendonça <gabrielmendoncanf@gmail.com>
2022-12-17 15:00:34 -0300

    Adicionar assets e o primeiro componente

---
+++ src/assets/js/app.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+export const App:FC = () => {
+  return <>
+    <h1> Hello world</h1>
+  </>; 
+};
---
+++ src/assets/js/main.tsx
@@ -0,0 +1,11 @@
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+import { App } from './app';
+
+import React from 'react';
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
