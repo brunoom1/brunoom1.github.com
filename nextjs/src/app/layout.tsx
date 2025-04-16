683e665cd06d8a33f0378883b540788bbdc6a7ef
Gabriel Mendonça <gabrielmendoncanf@gmail.com>
2025-04-16 20:16:46 -0300

    Add tagmanager

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -8,6 +8,7 @@ export const metadata: Metadata = {
 
 import './global.css';
 import { Header } from "@/components/Header";
+import Script from "next/script";
 
 const roboto = Roboto({
   weight: '300',
@@ -21,6 +22,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt_BR">
+      <head>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KWB57XDB');`
+          }}
+          id="gtm" /> 
+      </head>
       <body className={ roboto.className }>
         <div className="App">
           <Header />
